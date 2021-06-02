df43aa9988302f8482836ca048c15d307246b382
Partha Sarathi Dash <65017143+Partha-debug@users.noreply.github.com>
2021-06-02 19:15:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ def pswd_check_main():
     strength_profiles = pswd_checker.check_pswd(provided_pswd)
 
     if (strength_profiles['lnth'] >= 8) & (strength_profiles['smal'] >=2) & (strength_profiles['cptl'] >=2) & (strength_profiles['num'] >=2) & (strength_profiles['spcl_chr'] >=2):
-        print(f"Your password is strong it is of {strength_profiles['lnth']} characters contains {strength_profiles['smal']} small {strength_profiles['cptl']} capital {strength_profiles['num']} numeric and {strength_profiles['spcl_chr']} special characters.") 
+        print(f"\n[+]Your password is strong it is of {strength_profiles['lnth']} characters contains {strength_profiles['smal']} small {strength_profiles['cptl']} capital {strength_profiles['num']} numeric and {strength_profiles['spcl_chr']} special characters.") 
 
     elif (strength_profiles['lnth'] >= 6) & (strength_profiles['smal'] >=1) & (strength_profiles['cptl'] >=1) & (strength_profiles['num'] >=1) & (strength_profiles['spcl_chr'] >=1):
-        print(f"[-] Your password is of medium strength it is of {strength_profiles['lnth']} characters contains {strength_profiles['smal']} small {strength_profiles['cptl']} capital {strength_profiles['num']} numeric and {strength_profiles['spcl_chr']} special characters.\n\n[+] A Strong password must contain atleast 2 capital, 2 small,2 numeric and 2 special characters...") 
+        print(f"\n[-] Your password is of medium strength it is of {strength_profiles['lnth']} characters contains {strength_profiles['smal']} small {strength_profiles['cptl']} capital {strength_profiles['num']} numeric and {strength_profiles['spcl_chr']} special characters.\n\n[+] A Strong password must contain atleast 2 capital, 2 small,2 numeric and 2 special characters...") 
     else:
-        print(f"[-] Your password is weak it is of {strength_profiles['lnth']} characters contains {strength_profiles['smal']} small {strength_profiles['cptl']} capital {strength_profiles['num']} numeric and {strength_profiles['spcl_chr']} special characters.\n\n[+]A Strong password must contain atleast 2 capital, 2 small,2 numeric and 2 special characters...")
+        print(f"\n[-] Your password is weak it is of {strength_profiles['lnth']} characters contains {strength_profiles['smal']} small {strength_profiles['cptl']} capital {strength_profiles['num']} numeric and {strength_profiles['spcl_chr']} special characters.\n\n[+]A Strong password must contain atleast 2 capital, 2 small,2 numeric and 2 special characters...")
 
 
 def pwned_check_main():
@@ -43,9 +43,9 @@ def pwned_check_main():
         pwned_info = pwned_checker.is_pwned(provided_pswd)
 
         if pwned_info:
-            print(f"Your password was found in {pwned_info} hacked password data bases, change it as soon as possible...")
+            print(f"\n[-] Your password was found in {pwned_info} hacked password data bases, change it as soon as possible...")
         else:
-            print("You got a secure password, There is no instance of this password in the hacked password databases...")
+            print("\n[+] You got a secure password, There is no instance of this password in the hacked password databases...")
     except ConnectionError:
         print("There occured some error while connecting, Please check your network connection and try again!")
 
@@ -75,14 +75,14 @@ def pswd_gen_main():
             "You can't leave this field blank, please enter the website where you want to use this password: ").strip()
 
     print(
-        f"Generating a strong password with {provided_length} characters... ")
+        f"\n[+] Generating a strong password with {provided_length} characters... ")
 
 
     pswd_manager = Password_manager()
     generated_pswd = pswd_manager.strong_pswd(provided_length, used_site)
 
     print(
-        f"Your generated {used_site} password is '{generated_pswd}', It will be saved in the passwords.txt file")
+        f"\n[+] Your generated {used_site} password is '{generated_pswd}', It will be saved in the passwords.txt file")
 
 
 def main():
@@ -96,12 +96,46 @@ def main():
 
     if choice == '1':
         pswd_check_main()
+
+        print("\nHit enter to go back to home screen")
+        print("Enter any other key to exit.")
+        
+        opt = input()
+        
+        if opt == '':
+            main()
+        else:
+            raise KeyboardInterrupt
+
     elif choice == '2':
         pwned_check_main()
+
+        print("\nHit enter to go back to home screen")
+        print("Enter any other key to exit.")
+        
+        opt = input()
+        
+        if opt == '':
+            main()
+        else:
+            raise KeyboardInterrupt
+
     elif choice == '3':
         pswd_gen_main()
+
+        print("\nHit enter to go back to home screen.")
+        print("Enter any other key to exit.")
+        
+        opt = input()
+        
+        if opt == '':
+            print('\n')
+            main()
+        else:
+            raise KeyboardInterrupt
     else:
         print("Invalid choice please try again!")
+        print('\n')
         main()
 
 
@@ -111,7 +145,7 @@ if __name__ == "__main__":
         show_logo()
         main()
     except KeyboardInterrupt:
-        print("Exiting...")
+        print("Exiting...\n")
     except Exception as e:
         print(f"Some error occured, error details: {e}")
     
